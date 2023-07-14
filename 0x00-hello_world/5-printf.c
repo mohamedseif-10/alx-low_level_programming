9920497d06237ad46275cef7e6b3fbd786e69c03
mohamedseif-10 <mohamedseif.a1@gmail.com>
2023-07-13 19:43:42 -0700

    hello c

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,14 @@
+#include <iostream>
+
+/**
+ * main - Entry point
+ * 
+ * Description : A C programe that print string by function printf
+ *
+ * Return : it will return 0 if successed*/
+
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
