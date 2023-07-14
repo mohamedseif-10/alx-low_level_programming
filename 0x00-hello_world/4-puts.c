d8e4dd617a266d1a75d54cd53d262330996f6c28
mohamedseif-10 <mohamedseif.a1@gmail.com>
2023-07-13 17:16:26 -0700

    hello c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point 
+ *
+ * Description : a C programe that prints with puts function 
+ *
+ * Rerutn : 0 (for success)
+*/
+
+int main (void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return 0;
+}
