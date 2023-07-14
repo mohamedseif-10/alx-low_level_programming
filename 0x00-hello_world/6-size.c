8d129bb9b329b95fa6817f2e8f27b2f573325916
mohamedseif-10 <mohamedseif.a1@gmail.com>
2023-07-14 06:07:23 -0700

    hello c

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -10,10 +10,10 @@
 
 int mian(void)
 {
-	printf("size if a char: %lu byte(s) \n", sizeof(char));
-	printf("size if an int: %lu byte(s) \n", sizeof(int));
-	printf("size if a long int: %lu byte(s) \n", sizeof(long int));
-	printf("size if a long long int: %lu byte(s) \n", sizeof(long long int));
-	printf("size if a float: %lu byte(s) \n", sizeof(float));
+	printf("size if a char: %lu byte(s)\n", sizeof(char));
+	printf("size if an int: %lu byte(s)\n", sizeof(int));
+	printf("size if a long int: %lu byte(s)\n", sizeof(long int));
+	printf("size if a long long int: %lu byte(s)\n", sizeof(long long int));
+	printf("size if a float: %lu byte(s)\n", sizeof(float));
 	return (0);
 }
