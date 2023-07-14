e992151ab3b2c87c95514ab26255d7441ff4dc5c
mohamedseif-10 <mohamedseif.a1@gmail.com>
2023-07-13 20:04:08 -0700

    hello c

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry 
+ *
+ * Description: this a file
+ *
+ * Return: Always 0 (success)
+*/
+
+int mian(void)
+{
+	 printf("size if a char: %lu byte(s)\ n",sizeof(char));
+	 printf("size if an int: %lu byte(s)\ n",sizeof(int));
+	 printf("size if a long int: %lu byte(s)\ n",sizeof(long int));
+	 printf("size if a long long int: %lu byte(s)\ n",sizeof(long long int));
+	 printf("size if a float: %lu byte(s)\ n",sizeof(float));
+	 return (0);
+}
