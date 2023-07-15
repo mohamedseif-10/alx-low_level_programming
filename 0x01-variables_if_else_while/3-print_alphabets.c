4fe80c49581924431a114f9e8101b93d3ebc4647
mohamedseif-10 <mohamedseif.a1@gmail.com>
2023-07-15 12:48:54 -0700

    this is c task

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: this function prints all alphabet
+ *
+ * Return: 0 if (success)
+*/
+
+int main(void)
+{
+	int i = 97;
+
+	while (i < 123)
+	{
+		putchar(i);
+		i++;
+		if (i == 122)
+			i = 65;
+		if (i == 90)
+			break;
+	}
+	putchar('\n');
+	return (0);
+}
