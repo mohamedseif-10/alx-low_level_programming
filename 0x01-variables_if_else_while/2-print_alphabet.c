c5a0fc20f530949bd509b3ec02027cd066bcff90
mohamedseif-10 <mohamedseif.a1@gmail.com>
2023-07-15 12:28:41 -0700

    this is c task

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: this function prints all alphabet
+ *
+ * Return: 0 if (success)
+*/
+
+int main(void)
+{
+	for (size_t i =97;i<123;i++)
+		putchar(i);
+	return (0);
+}
