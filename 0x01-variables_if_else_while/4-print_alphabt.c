58de866d34ad0d77faae82f19de77adb1c137300
mohamedseif-10 <mohamedseif.a1@gmail.com>
2023-07-17 07:52:55 -0700

    this is c task

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,24 @@
+#include <iostream>
+
+/**
+ * main -Entry
+ *
+ * Description: prints all alphabet except e and q
+ *
+ * Return: Always 0 (if success)
+*/
+
+int main(void)
+{
+	char n = 'a';
+
+	while (n < 123)
+	{
+		if (n != 101 && n != 113)
+			putchar(n);
+		else
+			continue;
+	}
+	putchar('\n');
+	return (0);
+}
