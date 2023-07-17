468d2357b11460f76b7cee284f7d541c27638194
mohamedseif-10 <mohamedseif.a1@gmail.com>
2023-07-17 14:03:44 -0700

    this is c task

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: print digits from zero to 9
+ *
+ * Return: Always 0 (if succcess)
+*/
+
+int main(void)
+{
+	int i = 0;
+
+
+	while (i < 10)
+	{
+		putchar(n);
+		putchar(',');
+		putchar(' ')
+		i++;
+	}
+	return (0);
+}
