13d5de2d08368bee42a2dcd71b8da63b0d127ef5
mohamedseif-10 <mohamedseif.a1@gmail.com>
2023-07-17 08:17:52 -0700

    this is c task

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: print digits from zero to 9
+ *
+ * Return: Always 0 (if succcess)
+*/
+
+int main(void)
+{
+	int i = 47;
+
+	while (++i < 58)
+		putchar(i);
+	putchar('\n');
+	return (0);
+}
