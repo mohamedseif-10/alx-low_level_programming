0978efb86e8c5e3a5900b988d4707006b0f2ec2f
mohamedseif-10 <mohamedseif.a1@gmail.com>
2023-07-17 19:50:55 -0700

    this is c task

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+
+/**
+ * main - Entry
+ *
+ * Description: print digits from zero to 9
+ *
+ * Return: Always 0 (if succcess)
+*/
+
+int main(void)
+{
+	int i = 48, j = 49, l = 50, n = 0, x = 0;
+
+	while (i < 56)
+	{
+
+		while (j < 58)
+		{
+			while (l < 58)
+			{
+				putchar(i);
+				putchar(j);
+				if (i != 55)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+				l++;
+			}
+			x++;
+			l = 50 + x;
+			j++;
+		}
+		n++;
+		j = 49 + n;
+		i++;
+	}
+	putchar('\n');
+	return (0);
+}
